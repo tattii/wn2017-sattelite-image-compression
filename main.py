5610437226b7ab40a9030280d333490ee54ee075
tattii <yuta.tatti@gmail.com>
2017-12-16 13:37:07 +0900

    original encode format filename, data(deflated)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import zipfile
+import zlib
+import struct
 
 def encode():
     encodefile = input()
@@ -6,15 +7,45 @@ def encode():
     N = int(input())
     files = [input().split()[0] for _ in range(N)] # filename filesize
 
-    with zipfile.ZipFile(encodefile, 'w', zipfile.ZIP_BZIP2) as zf:
-        for name in files:
-            zf.write(directory + '/' + name, name)
+    encode_files(encodefile, directory, files)
+
+def encode_files(encodefile, directory, files):
+    with open(encodefile, 'wb') as f:
+        for file in files:
+            f.write(encode_file(directory, file))
+        
+
+def encode_file(directory, file):
+    with open(directory + '/' + file, 'rb') as f:
+        data = zlib.compress(f.read(), level=-1)
+    
+        header = struct.pack('B', len(file)) + file.encode('utf-8') + struct.pack('I', len(data))
+        return header + data
 
 def decode():
     encodefile = input()
     directory = input()
-    with zipfile.ZipFile(encodefile, 'r') as zf:
-        zf.extractall(directory)
+
+    decode_files(encodefile, directory)
+
+def decode_files(encodefile, directory):
+    with open(encodefile, 'rb') as f:
+        while True:
+            h = f.read(1)
+            if not h: break
+            decode_file(directory, f, h)
+        
+
+def decode_file(directory, f, h):
+    filename_size = struct.unpack('B', h)[0]
+    filename = f.read(filename_size).decode('utf-8')
+    print(filename)
+
+    data_size = struct.unpack('I', f.read(4))[0]
+    data = zlib.decompress(f.read(data_size))
+
+    with open(directory + '/' + filename, 'wb') as out:
+        out.write(data)
 
 
 command = input()
