a6f370af9de109a659d80f0fd283a24da9675bd1
tattii <yuta.tatti@gmail.com>
2018-01-15 20:12:57 +0900

    decode (not work)

--- go/rangecoder.go
+++ go/rangecoder.go
@@ -18,16 +18,28 @@ func main(){
 
 	if flag == "encode" {
 		encode_file(input, output)
+	}else if flag == "decode" {
+		decode_file(input, output)
 	}
-
 }
 
+
+//---------------------------------------------------------
+// encode
+//---------------------------------------------------------
 func encode_file(input, output string){
 	size, array := readBinary(input)
 
+	// sub
+	//array2 := make([]uint16, len(array))
+	//array2[0] = array[0] + 1024
+	//for i := 1; i < len(array); i++ {
+	//	array2[i] = array[i] - array[i - 1] + 1024
+	//}
+
 	out, _ := os.Create(output)
-	defer out.Close()
-	binary.Write(out, binary.LittleEndian, size)
+	//defer out.Close()
+	binary.Write(out, binary.LittleEndian, size / 2)
 
 	fmt.Println(size)
 	encode(array, out)
@@ -180,6 +192,7 @@ func (p *Encoder) finish(){
 		p.puti(c)
 	}
 
+	fmt.Println(p.low)
 	binary.Write(p.file, binary.LittleEndian, p.low)
 }
 
@@ -199,4 +212,126 @@ func readBinary(filename string) (size int64, array []uint16){
 }
 
 
+//---------------------------------------------------------
+// decode
+//---------------------------------------------------------
+func decode_file(input, output string){
+	out, _ := os.Create(output)
+	defer out.Close()
+
+	decode(out, input)
+}
+
+func read_encoded(filename string) (int64, []uint8, []uint16){
+	fileinfo, _ := os.Stat(filename)
+	filesize := fileinfo.Size()
+
+	file, _ := os.Open(filename)
+
+	var size int64
+	binary.Read(file, binary.LittleEndian, &size)
+
+	count_table := read_count_table(file)
+
+	datasize := int(filesize) - 8 - 2 * (len(count_table) + 1)
+	fmt.Println(datasize)
+	data := make([]uint8, datasize)
+	err := binary.Read(file, binary.LittleEndian, &data)
+	if err != nil {
+		fmt.Println("binary.Read failed:", err)
+	}
+
+	return size, data, count_table
+}
+
+func read_count_table(file *os.File) (count_table []uint16){
+	var maxi uint16
+	binary.Read(file, binary.LittleEndian, &maxi)
+
+	count_table = make([]uint16, maxi)
+	binary.Read(file, binary.LittleEndian, &count_table)
+	return
+}
+
+func decode(out *os.File, input string){
+	size, data, count_table := read_encoded(input)
+
+	decoder := &Decoder{}
+	decoder.init(data)
+	decoder.init_count_table(count_table)
+
+	fmt.Println(size)
+	for i := 0; i < int(size); i++ {
+		code := decoder.decode()
+		binary.Write(out, binary.LittleEndian, code)
+	}
+}
+
+type Decoder struct {
+	data []uint8
+	readi int
+	rng uint32
+	low uint32
+	count []uint16
+	count_sum []uint32
+	count_all float64
+}
+
+func (p *Decoder) init(data []uint8){
+	p.data = data
+	p.rng = MAX_RANGE
+
+	p.low = (((((uint32(data[1]) << 8) + uint32(data[2])) << 8) + uint32(data[3])) << 8) + uint32(data[4])
+	p.readi = 4
+}
+
+func (p *Decoder) init_count_table(count []uint16){
+	p.count = count
+
+	// sum count
+	p.count_sum = make([]uint32, len(count) + 1)
+	for i, x := range p.count {
+		p.count_sum[i + 1] = p.count_sum[i] + uint32(x)
+	}
+	p.count_all = float64(p.count_sum[len(p.count_sum) - 1])
+}
+
+func (p *Decoder) decode() uint16 {
+	temp := float64(p.rng)  / p.count_all
+	code := p.search_code(uint32(float64(p.low) / temp))
+	p.low -= uint32(temp * float64(p.count_sum[code]))
+	p.rng = uint32(temp * float64(p.count[code]))
+	p.normalize()
+
+	return uint16(code)
+}
+
+func (p *Decoder) search_code(value uint32) int {
+	i := 0
+	j := len(p.count) - 1
+	for i < j {
+		med := int((i + j) / 2)
+		if p.count_sum[med + 1] <= value {
+			i = med + 1
+		}else{
+			j = med
+		}
+	}
+	return i
+}
+
+func (p *Decoder) normalize() {
+	for p.rng < MIN_RANGE {
+		p.rng <<= 8
+		p.readi += 1
+		if p.readi >= len(p.data) {
+			p.low = ((p.low << 8) + 0) & MASK
+			return
+
+		}else{
+			c := p.data[p.readi]
+			p.low = ((p.low << 8) + uint32(c)) & MASK
+		}
+	}
+}
 
